c34c0472ca947854a5d58bab9622aee9c9930bca
Maximilian <scientificresearch309@gmail.com>
2023-06-27 14:42:41 +0200

    Ternary operator for both states

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,20 +1,29 @@
 const ListGroup = () => {
-	// const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
-	const items: [] = []; // ein leeres Array
+	const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+	// const items: [] = []; // ein leeres Array
 
-	if (items.length === 0) {
-		return <h1>No item found!</h1>;
-	}
+	// if (items.length === 0) {
+	// 	return (
+	// 		<>
+	// 			<h1>List</h1>
+	// 			<h1>No item found!</h1>;
+	// 		</>
+	// 	);
+	// }
 	return (
 		<>
 			<h1>List</h1>
-			<ul className="list-group">
-				{items.map((city, i) => (
-					<li className="list-group-item" key={i}>
-						{city}
-					</li>
-				))}
-			</ul>
+			{items.length === 0 ? (
+				<h1>No item found!</h1>
+			) : (
+				<ul className="list-group">
+					{items.map((city, i) => (
+						<li className="list-group-item" key={i}>
+							{city}
+						</li>
+					))}
+				</ul>
+			)}
 		</>
 	);
 };
