3cc26f9bc4df710d4e8d4037032e156dc0445925
Maximilian <scientificresearch309@gmail.com>
2023-06-27 15:20:42 +0200

    applying && instead of ternary operator!

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,6 +1,6 @@
 const ListGroup = () => {
 	const items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
-	// const items: [] = []; // ein leeres Array
+	//const items: [] = []; // ein leeres Array
 
 	// if (items.length === 0) {
 	// 	return (
@@ -13,17 +13,14 @@ const ListGroup = () => {
 	return (
 		<>
 			<h1>List</h1>
-			{items.length === 0 ? (
-				<h1>No item found!</h1>
-			) : (
-				<ul className="list-group">
-					{items.map((city, i) => (
-						<li className="list-group-item" key={i}>
-							{city}
-						</li>
-					))}
-				</ul>
-			)}
+			{items.length === 0 && <h1>No item found!</h1>}
+			<ul className="list-group">
+				{items.map((city, i) => (
+					<li className="list-group-item" key={i}>
+						{city}
+					</li>
+				))}
+			</ul>
 		</>
 	);
 };
