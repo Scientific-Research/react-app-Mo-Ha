4914d73fce4d053684f9e32676d1a98aebc0a7be
Maximilian <scientificresearch309@gmail.com>
2023-06-30 17:12:45 +0200

    <React.StrictMode> deactivated to prevent double checking!

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Like from './components/Like';
+import Message from './Message';
 
 const App = () => {
 	// const [game, setGame] = useState({
@@ -9,80 +10,81 @@ const App = () => {
 	// 	},
 	// });
 
-	interface Ipizza {
-		name: string;
-		toppings: string;
-	}
-	const [pizza, setPizza] = useState({
-		name: 'Spicy Pepperoni',
-		toppings: ['Mushroom'],
-	});
+	// interface Ipizza {
+	// 	name: string;
+	// 	toppings: string;
+	// }
+	// const [pizza, setPizza] = useState({
+	// 	name: 'Spicy Pepperoni',
+	// 	toppings: ['Mushroom'],
+	// });
 
-	const [cart, setCart] = useState({
-		discount: 0.1,
-		items: [
-			{ id: 1, title: 'Product 1', quantity: 1 },
-			{ id: 2, title: 'Product 2', quantity: 1 },
-		],
-	});
+	// const [cart, setCart] = useState({
+	// 	discount: 0.1,
+	// 	items: [
+	// 		{ id: 1, title: 'Product 1', quantity: 1 },
+	// 		{ id: 2, title: 'Product 2', quantity: 1 },
+	// 	],
+	// });
 
-	const handleClick = () => {
-		// Ex 3 Solution
-		setCart({
-			...cart,
-			items: [
-				...cart.items.map((item) =>
-					item.id === 1
-						? { ...item, quantity: item.quantity + 1 }
-						: item
-				),
-			],
-		});
-		console.log(cart.items);
+	// const handleClick = () => {
+	// 	// Ex 3 Solution
+	// 	setCart({
+	// 		...cart,
+	// 		items: [
+	// 			...cart.items.map((item) =>
+	// 				item.id === 1
+	// 					? { ...item, quantity: item.quantity + 1 }
+	// 					: item
+	// 			),
+	// 		],
+	// 	});
+	// 	console.log(cart.items);
 
-		// Ex 3 Solution
-		// setCart({
-		// 	...cart,
-		// 	items: cart.items.map((item) =>
-		// 		item.id == 1 ? { ...item, quantity: item.quantity + 1 } : item
-		// 	),
-		// });
-		// console.log(cart.items);
+	// Ex 3 Solution
+	// setCart({
+	// 	...cart,
+	// 	items: cart.items.map((item) =>
+	// 		item.id == 1 ? { ...item, quantity: item.quantity + 1 } : item
+	// 	),
+	// });
+	// console.log(cart.items);
 
-		// // SOLUTION_2
-		// setPizza({
-		// 	...pizza,
-		// 	toppings: [...pizza.toppings, 'Cheese', 'Salami'],
-		// });
-		// console.log(pizza.toppings);
-		// // SOLUTION_2
+	// // SOLUTION_2
+	// setPizza({
+	// 	...pizza,
+	// 	toppings: [...pizza.toppings, 'Cheese', 'Salami'],
+	// });
+	// console.log(pizza.toppings);
+	// // SOLUTION_2
 
-		// const newName = (game.player.name = 'Bob');
-		// const newName = game.player.name;
+	// const newName = (game.player.name = 'Bob');
+	// const newName = game.player.name;
 
-		// setGame({game.player.name = 'Bob'});
-		// setGame([game.player.name]='Bob');
-		// setGame({ ...game, player: { name: 'Bob' } });
-		// setGame({ ...game, player: { ...game.player, name: 'Bob' } });
+	// setGame({game.player.name = 'Bob'});
+	// setGame([game.player.name]='Bob');
+	// setGame({ ...game, player: { name: 'Bob' } });
+	// setGame({ ...game, player: { ...game.player, name: 'Bob' } });
 
-		// console.log(game.player.name);
-		// pizza.toppings.push('Cheese');
+	// console.log(game.player.name);
+	// pizza.toppings.push('Cheese');
 
-		// // Solution_1
-		// const Topping: any = ['Cheese'];
-		// Topping.forEach((element: any) => {
-		// 	pizza.toppings.push(element);
-		// });
-		// // setPizza({ ...pizza, toppings: ['Cheese'] });
-		// setPizza({ ...pizza });
-		// console.log(pizza.toppings);
-		// // Solution_1
-	};
+	// // Solution_1
+	// const Topping: any = ['Cheese'];
+	// Topping.forEach((element: any) => {
+	// 	pizza.toppings.push(element);
+	// });
+	// // setPizza({ ...pizza, toppings: ['Cheese'] });
+	// setPizza({ ...pizza });
+	// console.log(pizza.toppings);
+	// // Solution_1
+	//};
 
 	return (
 		<div>
-			<Like onclick={() => console.log('Clicked!')}></Like>
-			<button onClick={() => handleClick()}>My Button</button>
+			{/* <Like onclick={() => console.log('Clicked!')}></Like>
+			<button onClick={() => handleClick()}>My Button</button> */}
+			<Message />
 		</div>
 	);
 };
--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,13 @@
+let count = 0;
 const Message = () => {
-	const name = 'Maximilian';
-	if (name) return <h1>Hello {name}</h1>;
-	return <h1>There is nothing to show you here!</h1>;
+	// const name = 'Maximilian';
+	// if (name) return <h1>Hello {name}</h1>;
+	// return <h1>There is nothing to show you here!</h1>;
+	count++;
+	console.log(count);
+
+	return <div>Message {count}</div>;
+	// return count;
 };
 
 export default Message;
